7ffd2f34d5eeef949452117f23d0359d82fde24c
vhdcpatel <vrajhpatel21@gmail.com>
2024-06-25 09:51:15 +0530

    Removed the dummy test code and clear basics project.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [textVisible, setTextVisible] = useState(false);
 
-  const handleClick = () => {
-    setTextVisible(!textVisible);
-  }
 
   return (
     <div>
-      <h1>hello world!</h1>
-      {textVisible && <p>
-        hello from day1.
-      </p>}
-      <button onClick={handleClick}>
-        {textVisible ? 'Hide' : 'Show'}
-      </button>
+      <h1>hello world! day1</h1>
     </div>
   )
 }
