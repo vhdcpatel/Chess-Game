e8e0bea3275a9b6a833502bb77dfd2ac451bab90
vhdcpatel <vrajhpatel21@gmail.com>
2024-07-02 09:28:23 +0530

    Update the images to webp to png for removing background while moving.

--- frontend/src/utils/constants/srcMap.ts
+++ frontend/src/utils/constants/srcMap.ts
@@ -1,17 +1,17 @@
 // White Pieces
-import whiteRook from '../../assets/chessPieces/rookWhite.webp' 
-import whiteKnight from '../../assets/chessPieces/kingWhite.webp' 
-import whiteBishop from '../../assets/chessPieces/bishopWhite.webp' 
-import whiteQueen from '../../assets/chessPieces/queenWhite.webp' 
-import whiteKing from '../../assets/chessPieces/kingWhite.webp' 
-import whitePawn from '../../assets/chessPieces/pawnWhite.webp' 
+import whiteRook from '../../assets/chessPieces/rookWhite.png' 
+import whiteKnight from '../../assets/chessPieces/kingWhite.png' 
+import whiteBishop from '../../assets/chessPieces/bishopWhite.png' 
+import whiteQueen from '../../assets/chessPieces/queenWhite.png' 
+import whiteKing from '../../assets/chessPieces/kingWhite.png' 
+import whitePawn from '../../assets/chessPieces/pawnWhite.png' 
 // Black Pieces
-import blackRook from '../../assets/chessPieces/rookBlack.webp'
-import blackKnight from '../../assets/chessPieces/knightBlack.webp'
-import blackBishop from '../../assets/chessPieces/bishopBlack.webp'
-import blackQueen from '../../assets/chessPieces/queenBlack.webp'
-import blackKing from '../../assets/chessPieces/kingBlack.webp'
-import blackPawn from '../../assets/chessPieces/pawnBlack.webp'
+import blackRook from '../../assets/chessPieces/rookBlack.png'
+import blackKnight from '../../assets/chessPieces/knightBlack.png'
+import blackBishop from '../../assets/chessPieces/bishopBlack.png'
+import blackQueen from '../../assets/chessPieces/queenBlack.png'
+import blackKing from '../../assets/chessPieces/kingBlack.png'
+import blackPawn from '../../assets/chessPieces/pawnBlack.png'
 
 export type PieceColor = 'white' | 'black';
 export type PieceType = 'rook' | 'knight' | 'bishop' | 'queen' | 'king' | 'pawn';
