9ed5f76ba843099aec9ec1ff769501d417cdb1af
vhdcpatel <vrajhpatel21@gmail.com>
2024-07-09 21:32:17 +0530

    Completed setup for the MUI in the application.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from '../theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+  </ThemeProvider>
   // </React.StrictMode>,
 )
