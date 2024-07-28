c876ac4fda394c77e53e733f2bb1388e5df118d7
vhdcpatel <vrajhpatel21@gmail.com>
2024-07-28 20:53:37 +0530

    Handled redirect for already login user.

--- frontend/src/pages/authPage/AuthPage.tsx
+++ frontend/src/pages/authPage/AuthPage.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import AuthForm from "../../components/auth/AuthForm"
+import { useAuth } from "../../context/authContext/authContext";
+import styles from './AuthPage.module.css';
+import { useNavigate } from "react-router-dom";
 
 const AuthPage = () => {
+  // If user already logged in then redirect to HomePage.
+  const { isAuthenticated }= useAuth();
+  const navigator = useNavigate();
+  
+  useEffect(()=>{
+    if(isAuthenticated){
+      navigator('/');
+    }
+  },[])
   
   return (
     <>
-      <AuthForm/>
+      <div className={styles.container}>
+        <div className={styles.formWrapper}>
+          <AuthForm />
+        </div>
+      </div>
     </>
   )
 }
