7f8a14da38eef2d3baa1876ab79af36014cc423d
Vraj-ACE <vraj@acedataanalytics.com>
2025-06-26 11:21:30 +0530

    Started working on new UI from the layout.

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,16 +1,32 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom'
 import styles from './LayoutStyles.module.css'
 import HeaderBar from '../components/header/HeaderBar';
 
-const Layout = () => {
+const flagNew = false;
+
+const Layout = () => {    
     
     return (
-        <div className={styles.mainOuterCtn}>
-            <HeaderBar />
-            <main className={styles.mainBody}>
-                <Outlet />
-            </main>
-        </div>
+        flagNew ? (
+            <React.Fragment>
+                <div className={styles.mainOuterCtn}>
+                    <HeaderBar />
+                    <main className={styles.mainBody}>
+                        <Outlet />
+                    </main>
+                </div>
+            </React.Fragment>
+        ) : (
+            <React.Fragment>
+                <div className={styles.mainOuterCtnNew}>
+                    <aside className={styles.sidebar}>Sidebar</aside>
+                    <main className={styles.mainContent}>
+                        <Outlet />
+                    </main>
+                </div>
+            </React.Fragment>
+        )
     );
 };
 
