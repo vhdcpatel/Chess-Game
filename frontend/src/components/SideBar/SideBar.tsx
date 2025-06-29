986a01816249feceb8c16f5f988425b0b4a24a63
vhdcpatel <vrajhpatel21@gmail.com>
2025-06-29 11:49:04 +0530

    Updated UI for sidebar and done with the logo section.

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import styles from './SideBar.module.css'
+import ChessMainLogo from '../../assets/chessLogoMain.jpg';
 
 const SideBar = () => {
   return (
     <aside className={styles.sidebarOuterCtn}>
       <div className={styles.sidebarTop}>
-        <h1>Sidebar</h1>
-        {/* Logo / static top content */}
+        <div className={styles.sidebarLogoCtn}>
+          <img src={ChessMainLogo} alt="Chess Logo" className={styles.sidebarLogo} />
+          <span className={styles.sidebarLogoText}>Chess.JS</span>
+        </div>
       </div>
 
       <div className={styles.sidebarMiddle}>
+        <div className={styles.sidebarMenuCtn}>
+          Menu
+        </div>
+        {/* add option such as play or learn */}
+
         {/* This fills remaining space */}
       </div>
 
       <div className={styles.sidebarBottom}>
         {/* Use Profile goes here. */}
+        user Profile goes here
       </div>
     </aside>
   )
