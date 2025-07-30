1d283ae09f7de766ec96b6a05d83b1941b8b107d
Vraj-ACE <vraj@acedataanalytics.com>
2025-07-30 14:34:59 +0530

    Improve test coverage.

--- frontend/src/__tests__/utils/getGameResults/getGameResults.test.ts
+++ frontend/src/__tests__/utils/getGameResults/getGameResults.test.ts
@@ -8,13 +8,31 @@ describe('getGameResult', () => {
     expect(result.resultClass).toBeDefined();
   });
 
+  it('returns correct result for CheckMate (black turn)', () => {
+  const result = getGameResult('CheckMate', 'b', null);
+  expect(result.title).toBe('🏆 Game Over - Checkmate!');
+  expect(result.message).toBe('White wins by checkmate!');
+});
+
   it('returns correct result for Draw with reason', () => {
     const result = getGameResult('Draw', 'b', 'Repetition');
     expect(result.message).toBe('Repetition');
   });
 
+  it('returns correct result for Draw without reason', () => {
+  const result = getGameResult('Draw', 'w', null);
+  expect(result.message).toBe('The game ends in a draw.');
+});
+
+  it("should return stalemate for application.", ()=>{
+    const result = getGameResult('StaleMate','w');
+    expect(result.title).toBe("🤝 Game Over - Stalemate!");
+    expect(result.message).toBe("The game ends in a draw by stalemate.")
+
+  })
+
   it('returns correct default result for unknown game state', () => {
-    const result = getGameResult('Check', 'b', null); // 'Check' not handled
+    const result = getGameResult('Check', 'b'); // 'Check' not handled
     expect(result.title).toBe('🎯 Game Over');
   });
 });
